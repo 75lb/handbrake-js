3ffc37fd1856701ab31d526d1c2c64793aefb5df
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-03-04 17:59:50 +0000

    config-master changes, tidy up

--- cli.js
+++ cli.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
 
-var handbrake = require("./lib/handbrake");
+var handbrake = require("./lib/handbrake"),
+    util = require("util");
 
 process.argv.splice(0, 2);
 
 function log(){ 
-    console.log.apply(this, Array.prototype.slice.call(arguments)); 
+    process.stdout.write(util.format.apply(this, Array.prototype.slice.call(arguments)));
 }
 
 handbrake.run(process.argv)
     .on("output", log)
     .on("progress", function(encode){
-        var full = "encode: %d\% complete [%d fps, %d average fps, eta: %s]",
-            short = "encode: %d\% complete";
+        var full = "encode: %d\% complete [%d fps, %d average fps, eta: %s]\n",
+            short = "encode: %d\% complete\n";
         if(encode.fps){
             log(full, encode.percentComplete, encode.fps, encode.avgFps, encode.eta);
         } else {
             log(short, encode.percentComplete);
         }
     })
-    .on("complete", function(){ log("complete"); })
-    .on("terminated", function(){ log("terminated"); })
+    .on("complete", function(){ log("complete\n"); })
+    .on("terminated", function(){ log("terminated\n"); })
     .on("error", log);
