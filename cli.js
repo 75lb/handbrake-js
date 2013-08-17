44fca067a013397f89de8e1fbbc394cd8c42bdf3
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-08-17 16:52:24 +0100

    cli now uses create().. clients using run() won't receive 'error' events from HandbrakeOptions

--- cli.js
+++ cli.js
@@ -10,15 +10,15 @@ function red(txt){
     return "\x1b[31m" + txt + "\x1b[0m";
 }
 
-handbrake.run(process.argv)
+handbrake.create()
     .on("output", log)
-    .on("progress", function(encode){
+    .on("progress", function(progress){
         var full = "encode: %d\% complete [%d fps, %d average fps, eta: %s]",
             short = "encode: %d\% complete";
-        if(encode.fps){
-            log(full, encode.percentComplete, encode.fps, encode.avgFps, encode.eta);
+        if(progress.fps){
+            log(full, progress.percentComplete, progress.fps, progress.avgFps, progress.eta);
         } else {
-            log(short, encode.percentComplete);
+            log(short, progress.percentComplete);
         }
     })
     .on("complete", function(){ log("complete"); })
@@ -26,25 +26,5 @@ handbrake.run(process.argv)
     .on("error", function(err){
         log(red(err.message));
     })
-    .on("invalid", function(msg){
-        log(msg);
-    });
-
-// handbrake.create()
-//     .on("output", log)
-//     .on("progress", function(progress){
-//         var full = "encode: %d\% complete [%d fps, %d average fps, eta: %s]",
-//             short = "encode: %d\% complete";
-//         if(progress.fps){
-//             log(full, progress.percentComplete, progress.fps, progress.avgFps, progress.eta);
-//         } else {
-//             log(short, progress.percentComplete);
-//         }
-//     })
-//     .on("complete", function(){ log("complete"); })
-//     .on("terminated", function(){ log("terminated"); })
-//     .on("error", function(err){
-//         log(red(err.message));
-//     })
-//     .on("invalid", log)
-//     .run(process.argv);
+    .on("invalid", log)
+    .run(process.argv);
