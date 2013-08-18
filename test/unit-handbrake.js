62f555aa9ca5aff83451871dc91aa39071ec0565
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-08-18 16:01:09 +0100

    tests

--- test/unit-handbrake.js
+++ test/unit-handbrake.js
@@ -67,104 +67,101 @@ describe("handbrake", function(){
         
         it("should emit 'output' on ChildProcess stdout 'data'", function(done){
             var mockHandle = getHandle();
-            var dataReceived = "";
             handbrake.spawn()
                 .on("output", function(data){
-                    dataReceived = data; 
-                    assert.strictEqual(dataReceived, "test data");
+                    assert.strictEqual(data, "test data");
                     done();
                 });
             
             process.nextTick(function(){
                 mockHandle.stdout.emit("data", "test data");
                 mockHandle.emit("exit", 0);
-            })
+            });
         });
 
-        it("should emit 'output' on ChildProcess stderr 'data'", function(){
+        it("should emit 'output' on ChildProcess stderr 'data'", function(done){
             var mockHandle = getHandle();
-            var dataReceived = "";
             handbrake.spawn()
                 .on("output", function(data){
-                    dataReceived = data; 
+                    assert.strictEqual(data, "test data");
+                    done();
                 });
-                    
-            mockHandle.stderr.emit("data", "test data");
-            assert.strictEqual(dataReceived, "test data");
-            mockHandle.emit("exit", 0);
+        
+            process.nextTick(function(){
+                mockHandle.stderr.emit("data", "test data");
+                mockHandle.emit("exit", 0);
+            });
         });
 
-        it("should fire 'terminated' on killing ChildProcess", function(){
+        it("should fire 'terminated' on killing ChildProcess", function(done){
             var mockHandle = getHandle();
-            var eventFired = false;
 
             handbrake.spawn()
                 .on("terminated", function(){
-                    eventFired = true;
+                    assert.ok(true);
+                    done();
                 });
-            mockHandle.kill();
-                    
-            assert.ok(eventFired);
-        });
-
-        it("should fire 'error' on ChildProcess exit with non-zero code", function(){
-            var mockHandle = getHandle();
-            var eventFired = false;
-
-            var handle = handbrake.spawn();
-            handle.on("error", function(){
-                eventFired = true;
+            process.nextTick(function(){
+                mockHandle.kill();
             });
-            mockHandle.emit("exit", 1);
-                    
-            assert.ok(eventFired);
         });
 
-        it("should fire 'error' if ChildProcess outputs 'no title found'", function(){
+        it("should fire 'error' on ChildProcess exit with non-zero code", function(done){
             var mockHandle = getHandle();
-            var eventFired = false;
-
             handbrake.spawn()
                 .on("error", function(){
-                    eventFired = true;
+                    assert.ok(true);
+                    done();
                 });
-            mockHandle.stderr.emit("data", "No title found. Ho.");
-            mockHandle.emit("exit", 0);
+            process.nextTick(function(){
+                mockHandle.emit("exit", 1);
+            });
+        });
 
-            assert.ok(eventFired);
+        it("should fire 'error' if ChildProcess outputs 'no title found'", function(done){
+            var mockHandle = getHandle();
+            handbrake.spawn()
+                .on("error", function(){
+                    assert.ok(true);
+                    done();
+                });
+
+            process.nextTick(function(){
+                mockHandle.stderr.emit("data", "No title found. Ho.");
+                mockHandle.emit("exit", 0);
+            });
         });
     
-        it("should fire 'complete' if ChildProcess completes", function(){
+        it("should fire 'complete' if ChildProcess completes", function(done){
             var mockHandle = getHandle();
-            var eventFired = false;
-
             handbrake.spawn()
                 .on("complete", function(){
-                    eventFired = true;
+                    assert.ok(true);
+                    done();
                 });
-            mockHandle.emit("exit", 0);
 
-            assert.ok(eventFired);
+            process.nextTick(function(){
+                mockHandle.emit("exit", 0);
+            });
         });
         
-        it("should fire ('progress', progress) as ChildProcess encodes", function(){
+        it("should fire ('progress', progress) as ChildProcess encodes", function(done){
             var mockHandle = getHandle();
-            var progressData;
-            
             handbrake.spawn()
                 .on("progress", function(progress){
-                    progressData = progress;
+                    assert.deepEqual(progress, {
+                       percentComplete: 0.59,
+                       fps: 127.14,
+                       avgFps: 134.42,
+                       eta: "00h13m19s",
+                       task: "Encoding"
+                    });
+                    done();
                 });
-            mockHandle.stdout.emit("data", "Encoding: task 1 of 1, 0.59 % (127.14 fps, avg 134.42 fps, ETA 00h13m19s)");
-
-            assert.deepEqual(progressData, {
-               percentComplete: 0.59,
-               fps: 127.14,
-               avgFps: 134.42,
-               eta: "00h13m19s",
-               task: "Encoding"
+            process.nextTick(function(){
+                mockHandle.stdout.emit("data", "Encoding: task 1 of 1, 0.59 % (127.14 fps, avg 134.42 fps, ETA 00h13m19s)");
+                mockHandle.emit("exit", 0);
             });
-            mockHandle.emit("exit", 0);
         });
     });    
     
