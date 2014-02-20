f3a833781f196ea7888d51b83654d1709e651c34
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-02-20 23:58:37 +0000

    websocket example

--- examples/ws.js
+++ examples/ws.js
@@ -1,15 +1,23 @@
 /*
 Usage:
-$ node ws.js <any video file>
+$ node ws.js <regular handbrake options>
 */
 
 var WsServer = new require("ws-stream"),
-    fs = require("fs"),
     handbrake = require("../");
 
 var wsServer = new WsServer({ port: 4444 });
 
 wsServer.on("connect", function(stream){
-    var handle = handbrake.spawn({ input: process.argv[2], output: "output.mp4" });
-    handle.outputStream.pipe(stream);
+    handbrake.spawn(process.argv)
+        .on("terminated", function(){
+            console.log("terminated");
+        })
+        .on("error", function(err){
+            console.log(err.message);
+        })
+        .on("invalid", function(msg){
+            console.log(msg);
+        })
+        .outputStream.pipe(stream);
 });
