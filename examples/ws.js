7c486784996f09d62aea0e8e3f4531dd9daea054
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-02-22 17:35:36 +0000

    updated ws demo

--- examples/ws.js
+++ examples/ws.js
@@ -1,23 +1,33 @@
-/*
-Usage:
-$ node ws.js <regular handbrake options>
-*/
-
-var WsServer = new require("ws-stream"),
+var WsServer = new require("ws-server"),
     handbrake = require("../");
 
+/*
+Launch a websocket server on port 4444. 
+*/
 var wsServer = new WsServer({ port: 4444 });
 
-wsServer.on("connect", function(stream){
-    handbrake.spawn(process.argv)
-        .on("terminated", function(){
-            console.log("terminated");
-        })
-        .on("error", function(err){
-            console.log(err.message);
-        })
-        .on("invalid", function(msg){
-            console.log(msg);
-        })
-        .outputStream.pipe(stream);
+/*
+When a client connects, listen for the websocket to become readable then pass the received
+options to Handbrake. Pipe the handbrake outputStream back down the websocket to the client. 
+*/
+wsServer.on("connection", function(websocket){
+    websocket.on("readable", function(){
+        var chunk;
+        while((chunk = this.read()) !== null){
+            try{
+                var options = JSON.parse(chunk.toString());
+                handbrake.spawn(options)
+                    .on("terminated", function(){
+                        console.log("terminated");
+                    })
+                    .on("error", function(err){
+                        console.log(err.message);
+                    })
+                    .on("invalid", function(msg){
+                        console.log(msg);
+                    })
+                    .outputStream.pipe(websocket);
+            } catch(e){}
+        }
+    });
 });
