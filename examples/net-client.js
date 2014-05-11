d605bc6d2b0dca50e28881836b640c701fd2520e
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-11 17:40:13 +0200

    streaming API removed, will resurface in a separate module

--- examples/net-client.js
+++
@@ -1,5 +0,0 @@
-var net = require("net");
-    
-net.connect({ port: 3338 }, function(){
-    this.write(JSON.stringify({ input: "demo.mkv", output: "demo.mp4" }));
-}).pipe(process.stdout);
--- examples/net-server.js
+++
@@ -1,8 +0,0 @@
-var net = require("net"),
-    handbrake = require("../");
-
-process.chdir(__dirname);
-
-net.createServer(function(socket){
-    socket.pipe(handbrake.createStream()).pipe(socket);
-}).listen(3338);
