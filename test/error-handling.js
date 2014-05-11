56037ad0769190f2d6041d352134bd26b27c44fb
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-11 17:38:22 +0200

    HandbrakeCLIError test passing

--- test/error-handling.js
+++ test/error-handling.js
@@ -18,3 +18,20 @@ test("error handling: HandbrakeCLI not found", function(t){
         });
     });
 });
+
+test("error handling: HandbrakeCLIError", function(t){
+    handbrake._inject({ cp: mock_cp });
+    t.plan(1);
+
+    var handbrakeProcess = handbrake.spawn({ input: "blah", output: "blah" });
+    handbrakeProcess.on("error", function(err){
+        console.dir(err.message)
+        t.deepEqual(err, {
+            name: "HandbrakeCLIError",
+            message: "Handbrake failed with error code: 13",
+            errno: 13
+        });
+    });
+    
+    mock_cp.lastHandle.emit("exit", 13);
+});
