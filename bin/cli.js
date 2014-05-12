b08fb64b4ec80acc1b0ffcf6c47c826597fdc5aa
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-12 13:02:07 +0200

    cli fix

--- bin/cli.js
+++ bin/cli.js
@@ -10,26 +10,32 @@ var handbrakeOptions = new HandbrakeOptions();
 handbrakeOptions.set(process.argv);
 
 function onProgress(progress){
-    dope.column(1).write(progress.task);
+    dope.column(1).write(progress.task + "  ");
     dope.column(11).write(progress.percentComplete.toFixed(2) + "   ");
     dope.column(22).write(progress.fps.toFixed(2) + "   ");
     dope.column(32).write(progress.avgFps.toFixed(2) + "   ");
     dope.column(42).write(progress.eta);
 }
 
-var handbrakeProcess = handbrake.spawn(handbrakeOptions)
-    .on("error", function(err){
-        dope.red.log(err);
-    })
-    .on("complete", function(){
-        dope.log();
-    });
-
-if (handbrakeOptions.verbose){
-    handbrakeProcess.on("output", dope.write);
+if (handbrake.input && handbrake.output){
+    var handbrakeProcess = handbrake.spawn(handbrakeOptions)
+        .on("error", function(err){
+            dope.red.log(err);
+        })
+        .on("complete", function(){
+            dope.log();
+        });
+    
+    if (handbrakeOptions.verbose){
+        handbrakeProcess.on("output", dope.write);
+    } else {
+        handbrakeProcess.on("start", function(){
+            dope.bold.log("Task      % done     FPS       Avg FPS   ETA");
+        });    
+        handbrakeProcess.on("progress", onProgress)
+    }
 } else {
-    handbrakeProcess.on("start", function(){
-        dope.bold.log("Task      % done     FPS       Avg FPS   ETA");
-    });    
-    handbrakeProcess.on("progress", onProgress)
+    handbrakeProcess.on("output", dope.write);
+    handbrake.spawn({ help: true });
 }
+
---
+++ test/integration.js
@@ -0,0 +1,12 @@
+"use strict";
+var test = require("tape"),
+    cp = require("child_process");
+
+// also travis should test installation script
+
+// test("cli: no args", function(t){
+//     t.plan(1);
+//     cp.exec("handbrake", function(err, stdout, stderr){
+//         t.
+//     });
+// });
