7572f7e677c5d110a71c94df13ea4663044fdb33
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-13 00:32:52 +0200

    cli --preset-list test added

--- test/integration.js
+++ test/integration.js
@@ -3,9 +3,9 @@ var test = require("tape"),
     cp = require("child_process"),
     fs = require("fs");
 
-test("cli: no args", function(t){
+test("cli: --preset-list", function(t){
     t.plan(1);
-    cp.exec("node bin/cli.js", function(err, stdout, stderr){
+    cp.exec("node bin/cli.js --preset-list", function(err, stdout, stderr){
         if (err) {
             t.fail(stderr);
         } else {
