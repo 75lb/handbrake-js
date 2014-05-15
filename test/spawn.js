b1cd664d50fa96b6b5e2a5621cbd0e7bba7ebd7b
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-05-15 13:24:22 +0200

    removed Handbrake property from main module API

--- test/spawn.js
+++ test/spawn.js
@@ -1,10 +1,11 @@
 "use strict";
 var test = require("tape"),
     handbrakeJs = require("../lib/handbrake-js"),
+    Handbrake = require("../lib/Handbrake"),
     mockCp = require("./mock/child_process");
 
 test("spawn: correct return type", function(t){
     t.plan(1);
     var handbrake = handbrakeJs.spawn({ input: "blah", output: "blah" }, { cp: mockCp });
-    t.ok(handbrake instanceof handbrakeJs.Handbrake);
+    t.ok(handbrake instanceof Handbrake);
 });
