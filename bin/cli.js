cabc2416d39b6526c73bbc987e418bc14018d59e
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-03-23 11:56:15 +0000

    factor console-log out of project

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 'use strict'
-
-var dope = require('console-dope')
+var ansi = require('ansi-escape-sequences')
 var cliArgs = require('command-line-args')
 var hbjs = require('../lib/handbrake-js')
 var cliOptions = require('../lib/cli-options')
+var util = require('util')
 
 var cli = cliArgs(cliOptions)
 try {
@@ -13,16 +13,20 @@ try {
   halt(err)
 }
 
+function column (n, msg) {
+  process.stdout.write(ansi.cursor.horizontalAbsolute(n) + msg)
+}
+
 function onProgress (progress) {
-  dope.column(1).write(progress.task + '  ')
-  dope.column(11).write(progress.percentComplete.toFixed(2) + '   ')
-  dope.column(22).write(progress.fps.toFixed(2) + '   ')
-  dope.column(32).write(progress.avgFps.toFixed(2) + '   ')
-  dope.column(42).write(progress.eta)
+  column(1, progress.task + '  ')
+  column(11, progress.percentComplete.toFixed(2) + '   ')
+  column(22, progress.fps.toFixed(2) + '   ')
+  column(32, progress.avgFps.toFixed(2) + '   ')
+  column(42, progress.eta)
 }
 
 function halt (err) {
-  dope.red.error(err)
+  console.error(ansi.format(util.inspect(err), 'red'))
   process.exit(1)
 }
 
@@ -30,23 +34,23 @@ function halt (err) {
 if (handbrakeOptions.input && handbrakeOptions.output) {
   var handbrake = hbjs.spawn(handbrakeOptions)
     .on('error', halt)
-    .on('complete', dope.log)
+    .on('complete', console.log.bind(console))
 
   if (handbrakeOptions.verbose) {
-    handbrake.on('output', dope.write)
+    handbrake.on('output', process.stdout.write.bind(process.stdout))
   } else {
     handbrake
       .on('begin', function () {
-        dope.bold.log('Task      % done     FPS       Avg FPS   ETA')
+        console.log(ansi.format('Task      % done     FPS       Avg FPS   ETA', 'bold'))
         this.began = true
       })
       .on('progress', onProgress)
       .on('complete', function () {
-        if (!this.began) dope.error(this.output)
+        if (!this.began) console.error(this.output)
       })
   }
 } else {
   hbjs.spawn(handbrakeOptions)
     .on('error', halt)
-    .on('output', dope.write)
+    .on('output', process.stdout.write.bind(process.stdout))
 }
