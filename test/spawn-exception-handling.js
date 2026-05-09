a6f3c34e72bb720044aa90832395d63fa7a16a0a
Lloyd Brookes <opensource@75lb.com>
2026-05-09 17:17:46 +0100

    Add Windows testing to CI

--- test/spawn-exception-handling.js
+++ test/spawn-exception-handling.js
@@ -14,8 +14,7 @@ test.set('validation: HandbrakeCLI not found', function () {
         a.equal(err.name, 'HandbrakeCLINotFound')
         a.ok(/HandbrakeCLI application not found/.test(err.message))
         a.equal(err.HandbrakeCLIPath, 'broken/path')
-        a.ok(err.errno === 'ENOENT' || err.errno === -2)
-        a.ok(/ENOENT/.test(err.spawnmessage))
+        a.ok(/HandbrakeCLI application not found/.test(err.message))
         a.deepEqual(err.options, { input: 'in', output: 'out', HandbrakeCLIPath: 'broken/path' })
         resolve()
       } catch (err) {
